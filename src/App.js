7405dd734042b861384c79bdf15a5d5212e0bddb
Rade Dobras <rade_dobras@yahoo.com>
2025-04-29 20:02:54 +0200

    Poruka za commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+let background = "White";
+const hour = new Date().getHours();
+
+if(hour >= 21 || hour < 7) {
+  background = "Darkblue";
+} 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: background}}>
+      <p>"Pozdrav!"</p>
     </div>
-  );
+    
+  )
 }
 
 export default App;
